e040d842da5e7ba2222283b23a4f9a705b0f0076
IsmailOuali <144810840+IsmailOuali@users.noreply.github.com>
2025-10-21 12:34:45 +0100

    V3 Update

--- ssr.mjs
+++ ssr.mjs
@@ -13,7 +13,6 @@ import PropTypes from "prop-types";
 import { unstable_generateUtilityClasses, unstable_generateUtilityClass, unstable_composeClasses, unstable_useEnhancedEffect, unstable_useForkRef, unstable_ownerDocument, unstable_capitalize, unstable_useId, unstable_ownerWindow, unstable_debounce, unstable_useEventCallback, elementAcceptingRef, exactProp, unstable_getReactElementRef, unstable_setRef, HTMLElementType, refType as refType$1, chainPropTypes as chainPropTypes$1, usePreviousProps, unstable_useControlled, unstable_getScrollbarSize, unstable_createChainedFunction } from "@mui/utils";
 import _objectWithoutPropertiesLoose$1 from "@babel/runtime/helpers/esm/objectWithoutPropertiesLoose";
 import _toPropertyKey$1 from "@babel/runtime/helpers/esm/toPropertyKey";
-import { createSelector as createSelector$1, defaultMemoize } from "reselect";
 import require$$0 from "@babel/runtime/helpers/interopRequireDefault";
 import require$$1 from "@babel/runtime/helpers/extends";
 import require$$2 from "@babel/runtime/helpers/objectWithoutPropertiesLoose";
@@ -3461,6 +3460,184 @@ function getDataGridUtilityClass(slot) {
   return unstable_generateUtilityClass("MuiDataGrid", slot);
 }
 const gridClasses = unstable_generateUtilityClasses("MuiDataGrid", ["actionsCell", "aggregationColumnHeader", "aggregationColumnHeader--alignLeft", "aggregationColumnHeader--alignCenter", "aggregationColumnHeader--alignRight", "aggregationColumnHeaderLabel", "autoHeight", "autosizing", "booleanCell", "cell--editable", "cell--editing", "cell--textCenter", "cell--textLeft", "cell--textRight", "cell--withRenderer", "cell--rangeTop", "cell--rangeBottom", "cell--rangeLeft", "cell--rangeRight", "cell--selectionMode", "cell", "cellContent", "cellCheckbox", "cellSkeleton", "checkboxInput", "columnHeader--alignCenter", "columnHeader--alignLeft", "columnHeader--alignRight", "columnHeader--dragging", "columnHeader--moving", "columnHeader--numeric", "columnHeader--sortable", "columnHeader--sorted", "columnHeader--filtered", "columnHeader", "columnHeaderCheckbox", "columnHeaderDraggableContainer", "columnHeaderDropZone", "columnHeaderTitle", "columnHeaderTitleContainer", "columnHeaderTitleContainerContent", "columnGroupHeader", "columnHeader--filledGroup", "columnHeader--emptyGroup", "columnHeader--showColumnBorder", "columnHeaders", "columnHeadersInner", "columnHeadersInner--scrollable", "columnSeparator--resizable", "columnSeparator--resizing", "columnSeparator--sideLeft", "columnSeparator--sideRight", "columnSeparator", "columnsPanel", "columnsPanelRow", "detailPanel", "detailPanels", "detailPanelToggleCell", "detailPanelToggleCell--expanded", "footerCell", "panel", "panelHeader", "panelWrapper", "panelContent", "panelFooter", "paper", "editBooleanCell", "editInputCell", "filterForm", "filterFormDeleteIcon", "filterFormLogicOperatorInput", "filterFormColumnInput", "filterFormOperatorInput", "filterFormValueInput", "filterIcon", "footerContainer", "headerFilterRow", "iconButtonContainer", "iconSeparator", "main", "menu", "menuIcon", "menuIconButton", "menuOpen", "menuList", "overlay", "overlayWrapper", "overlayWrapperInner", "root", "root--densityStandard", "root--densityComfortable", "root--densityCompact", "root--disableUserSelection", "row", "row--editable", "row--editing", "row--lastVisible", "row--dragging", "row--dynamicHeight", "row--detailPanelExpanded", "rowReorderCellPlaceholder", "rowCount", "rowReorderCellContainer", "rowReorderCell", "rowReorderCell--draggable", "scrollArea--left", "scrollArea--right", "scrollArea", "selectedRowCount", "sortIcon", "toolbarContainer", "toolbarFilterList", "virtualScroller", "virtualScrollerContent", "virtualScrollerContent--overflowed", "virtualScrollerRenderZone", "pinnedColumns", "pinnedColumns--left", "pinnedColumns--right", "pinnedColumnHeaders", "pinnedColumnHeaders--left", "pinnedColumnHeaders--right", "withBorderColor", "cell--withRightBorder", "columnHeader--withRightBorder", "treeDataGroupingCell", "treeDataGroupingCellToggle", "groupingCriteriaCell", "groupingCriteriaCellToggle", "pinnedRows", "pinnedRows--top", "pinnedRows--bottom", "pinnedRowsRenderZone"]);
+var NOT_FOUND = "NOT_FOUND";
+function createSingletonCache(equals) {
+  var entry;
+  return {
+    get: function get(key) {
+      if (entry && equals(entry.key, key)) {
+        return entry.value;
+      }
+      return NOT_FOUND;
+    },
+    put: function put(key, value) {
+      entry = {
+        key,
+        value
+      };
+    },
+    getEntries: function getEntries() {
+      return entry ? [entry] : [];
+    },
+    clear: function clear() {
+      entry = void 0;
+    }
+  };
+}
+function createLruCache(maxSize, equals) {
+  var entries = [];
+  function get(key) {
+    var cacheIndex = entries.findIndex(function(entry2) {
+      return equals(key, entry2.key);
+    });
+    if (cacheIndex > -1) {
+      var entry = entries[cacheIndex];
+      if (cacheIndex > 0) {
+        entries.splice(cacheIndex, 1);
+        entries.unshift(entry);
+      }
+      return entry.value;
+    }
+    return NOT_FOUND;
+  }
+  function put(key, value) {
+    if (get(key) === NOT_FOUND) {
+      entries.unshift({
+        key,
+        value
+      });
+      if (entries.length > maxSize) {
+        entries.pop();
+      }
+    }
+  }
+  function getEntries() {
+    return entries;
+  }
+  function clear() {
+    entries = [];
+  }
+  return {
+    get,
+    put,
+    getEntries,
+    clear
+  };
+}
+var defaultEqualityCheck = function defaultEqualityCheck2(a, b) {
+  return a === b;
+};
+function createCacheKeyComparator(equalityCheck) {
+  return function areArgumentsShallowlyEqual(prev, next) {
+    if (prev === null || next === null || prev.length !== next.length) {
+      return false;
+    }
+    var length = prev.length;
+    for (var i2 = 0; i2 < length; i2++) {
+      if (!equalityCheck(prev[i2], next[i2])) {
+        return false;
+      }
+    }
+    return true;
+  };
+}
+function defaultMemoize(func, equalityCheckOrOptions) {
+  var providedOptions = typeof equalityCheckOrOptions === "object" ? equalityCheckOrOptions : {
+    equalityCheck: equalityCheckOrOptions
+  };
+  var _providedOptions$equa = providedOptions.equalityCheck, equalityCheck = _providedOptions$equa === void 0 ? defaultEqualityCheck : _providedOptions$equa, _providedOptions$maxS = providedOptions.maxSize, maxSize = _providedOptions$maxS === void 0 ? 1 : _providedOptions$maxS, resultEqualityCheck = providedOptions.resultEqualityCheck;
+  var comparator = createCacheKeyComparator(equalityCheck);
+  var cache = maxSize === 1 ? createSingletonCache(comparator) : createLruCache(maxSize, comparator);
+  function memoized() {
+    var value = cache.get(arguments);
+    if (value === NOT_FOUND) {
+      value = func.apply(null, arguments);
+      if (resultEqualityCheck) {
+        var entries = cache.getEntries();
+        var matchingEntry = entries.find(function(entry) {
+          return resultEqualityCheck(entry.value, value);
+        });
+        if (matchingEntry) {
+          value = matchingEntry.value;
+        }
+      }
+      cache.put(arguments, value);
+    }
+    return value;
+  }
+  memoized.clearCache = function() {
+    return cache.clear();
+  };
+  return memoized;
+}
+function getDependencies(funcs) {
+  var dependencies = Array.isArray(funcs[0]) ? funcs[0] : funcs;
+  if (!dependencies.every(function(dep) {
+    return typeof dep === "function";
+  })) {
+    var dependencyTypes = dependencies.map(function(dep) {
+      return typeof dep === "function" ? "function " + (dep.name || "unnamed") + "()" : typeof dep;
+    }).join(", ");
+    throw new Error("createSelector expects all input-selectors to be functions, but received the following types: [" + dependencyTypes + "]");
+  }
+  return dependencies;
+}
+function createSelectorCreator(memoize2) {
+  for (var _len = arguments.length, memoizeOptionsFromArgs = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    memoizeOptionsFromArgs[_key - 1] = arguments[_key];
+  }
+  var createSelector2 = function createSelector3() {
+    for (var _len2 = arguments.length, funcs = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      funcs[_key2] = arguments[_key2];
+    }
+    var _recomputations = 0;
+    var _lastResult;
+    var directlyPassedOptions = {
+      memoizeOptions: void 0
+    };
+    var resultFunc = funcs.pop();
+    if (typeof resultFunc === "object") {
+      directlyPassedOptions = resultFunc;
+      resultFunc = funcs.pop();
+    }
+    if (typeof resultFunc !== "function") {
+      throw new Error("createSelector expects an output function after the inputs, but received: [" + typeof resultFunc + "]");
+    }
+    var _directlyPassedOption = directlyPassedOptions, _directlyPassedOption2 = _directlyPassedOption.memoizeOptions, memoizeOptions = _directlyPassedOption2 === void 0 ? memoizeOptionsFromArgs : _directlyPassedOption2;
+    var finalMemoizeOptions = Array.isArray(memoizeOptions) ? memoizeOptions : [memoizeOptions];
+    var dependencies = getDependencies(funcs);
+    var memoizedResultFunc = memoize2.apply(void 0, [function recomputationWrapper() {
+      _recomputations++;
+      return resultFunc.apply(null, arguments);
+    }].concat(finalMemoizeOptions));
+    var selector = memoize2(function dependenciesChecker() {
+      var params = [];
+      var length = dependencies.length;
+      for (var i2 = 0; i2 < length; i2++) {
+        params.push(dependencies[i2].apply(null, arguments));
+      }
+      _lastResult = memoizedResultFunc.apply(null, params);
+      return _lastResult;
+    });
+    Object.assign(selector, {
+      resultFunc,
+      memoizedResultFunc,
+      dependencies,
+      lastResult: function lastResult() {
+        return _lastResult;
+      },
+      recomputations: function recomputations() {
+        return _recomputations;
+      },
+      resetRecomputations: function resetRecomputations() {
+        return _recomputations = 0;
+      }
+    });
+    return selector;
+  };
+  return createSelector2;
+}
+var createSelector$1 = /* @__PURE__ */ createSelectorCreator(defaultMemoize);
 const cacheContainer = {
   cache: /* @__PURE__ */ new WeakMap()
 };
